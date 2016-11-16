fa5a06ea5c68b9b029c4fd46ad9b6aaa96816b80
suraj1385@gmail.com <suraj1385@gmail.com>
2016-11-15 20:04:32 -0800

    core.java-1

---
+++ src/test/java/core/Core.java
@@ -0,0 +1,49 @@
+package core;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Core {
+
+	public static WebDriver driver = null;
+	public static Properties config = null;
+	public static Properties object = null;
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	public static void setDriver(WebDriver driver) {
+		Core.driver = driver;
+	}
+
+	public static WebDriverWait wait = null;
+	public static String timeStamp = null;
+
+	public static final Logger logger = Logger.getLogger("devpinoyLogger");
+
+	public static void startAutomation() {
+		FileInputStream fileIn = null;
+		try {
+
+			logger.info("Starting automation");
+			fileIn = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\properties\\Config.properties");
+			config.load(fileIn);
+			logger.info("Config properties file loaded");
+			fileIn = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\properties\\Object.properties");
+			object.load(fileIn);
+			logger.info("Object properties file loaded");
+		} catch (Exception e) {
+
+		}
+	}
+
+	public static void closeAutomation() {
+		driver.quit();
+		logger.info("Quiting the driver");
+	}
+
+}
